595ed4870345137b211dba36add8b07f37a743a7
ofry <speitshot@gmail.com>
2021-12-07 12:52:55 +0300

    Deploy docker

--- configs/db.py
+++ configs/db.py
@@ -1,3 +1,9 @@
 import os
 
-DB_URL = os.environ['DB_URL']
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = os.environ['DB_PORT']
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+DB_NAME = os.environ['DB_NAME']
+
+DB_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
