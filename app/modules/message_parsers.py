14b9642732d1d1fdf777d857b0a6a76639d22e7b
ofry <speitshot@gmail.com>
2022-01-05 18:19:14 +0300

    Добавлены парсеры сообщений пользователя
    - добавление новой записи
    - поиск по тегам

--- app/modules/message_parsers.py
+++ app/modules/message_parsers.py
@@ -1,9 +1,20 @@
 import re
 import typing
 
+from aiogram.types import Message
+
 from .user_record_module.schemas import Record
 
 TAGS_PATTERN = r'(#\w+)|(<>.+<>)'
+SEC_PATTERN = r'<>[\d\w\!@#$%^&*()<>]+<>'
+SEARCH_TAGS_PATTERN = r'\w+'
+DETECT_COMMAND = r'^\/[\w]+'
+
+
+def parse_security_data(message: str) -> typing.List[str]:
+    """Парсинг секретной информации"""
+    security_data = re.findall(SEC_PATTERN, message)
+    return serialize_security_data(security_data)
 
 
 def parse_tags(message: str) -> typing.List[str]:
@@ -23,7 +34,30 @@ def serialize_tags(tags: typing.List[str]) -> typing.List[str]:
     return handled_data
 
 
-def parse_new_record(message: str) -> Record:
+def serialize_security_data(security_data: typing.List[str]) -> typing.List[str]:
+    """Привести теги к единому формату"""
+    handled_data = []
+    for data in security_data:
+        if data:
+            data = data.strip()
+            data = data.replace('<>', '')
+            handled_data.append(data)
+    return handled_data
+
+
+def cut_command(message: str) -> str:
+    """Обрезание команды в строке"""
+    return re.sub(DETECT_COMMAND, '', message).strip()
+
+
+def parse_message__add_record(message: Message) -> Record:
     """Парсинг информации в сообщении"""
-    tags = parse_tags(message)
-    return Record(tags=tags, text=message)
+    html_text = cut_command(message.html_text)
+    tags = parse_tags(html_text)
+    return Record(tags=tags, text=html_text)
+
+
+def parse_message__search_by_tags(message: Message) -> typing.List[str]:
+    """Парсинг тегов в строке"""
+    sub_strings = re.findall(SEARCH_TAGS_PATTERN, message.text)
+    return sub_strings
