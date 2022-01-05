7b24c3ea587c82422d7346fa3fdc08b56e095383
ofry <speitshot@gmail.com>
2022-01-05 18:19:48 +0300

    Добавлены необходимые позитивные тесты для парсеров

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+from collections import namedtuple
+
 import pytest
 
 from app.modules.helpers import WrongFormat
+
+Message = namedtuple('Message', ['text', 'html_text'], defaults=[None])
 
 
 class TestData:
@@ -9,73 +13,6 @@ class TestData:
     def __init__(self, input_data, output_data):
         self.input = input_data
         self.output = output_data
-
-
-@pytest.fixture()
-def positive__messages_for_search_by_tags():
-    """Тестовые данные для поиска по тегам"""
-    return (
-        TestData('#SERVER', ['server']),
-        TestData('#SERVER #WORK', ['server', 'work']),
-        TestData('#SERVER#WORK', ['server', 'work']),
-        TestData('#SERVER##', ['server']),
-        TestData('server', ['server']),
-        TestData('serverhome', ['serverhome']),
-        TestData('server home', ['server', 'home']),
-        TestData('#server home #m9', ['server', 'home', 'm9']),
-    )
-
-
-@pytest.fixture()
-def positive__messages_for_parse_tags():
-    """Тестовые данные для поиска по тегам"""
-    return (
-        TestData(
-            '/add server work',
-            []
-        ),
-        TestData(
-            '/add #server #work',
-            ['server', 'work']
-        ),
-        TestData(
-            '/add #Server #Work',
-            ['server', 'work']
-        ),
-        TestData(
-            '/add #SERVER #WORK',
-            ['server', 'work']
-        ),
-        TestData(
-            '/add #server#work',
-            ['server', 'work']
-        ),
-        TestData(
-            '/add #server #work working server with apps',
-            ['server', 'work']
-        ),
-        TestData(
-            '/add #server #work working server with #test apps',
-            ['server', 'work', 'test']
-        ),
-        TestData(
-            '/add #server #work working server with #test apps username@megahost <>12345678<>',
-            ['server', 'work', 'test']
-        ),
-        TestData(
-            '/add #server #work working server with #test apps username@megahost <>1#234#5678<> #credential',
-            ['server', 'work', 'test', 'credential']
-        )
-    )
-
-
-@pytest.fixture()
-def negative__messages_for_search_by_tags():
-    """Тестовые данные для поиска по тегам"""
-    return (
-        TestData('/gets', WrongFormat),
-        TestData('server /add', WrongFormat)
-    )
 
 
 @pytest.fixture()
--- tests/units/test__parser_message.py
+++ tests/units/test__parser_message.py
@@ -1,10 +1,66 @@
-from app.modules.message_parsers import parse_tags
+import typing
+
+import pytest
+
+from app.modules import message_parsers
+from tests.conftest import Message
+
+testdata = \
+    [
+        (
+            '/add #server #work working server with #test apps username@megahost <>1#234#5678<> #credential',
+            ['server', 'work', 'test', 'credential']
+        ),
+        (
+            '/add #server #work working server with apps',
+            ['server', 'work']
+        )
+    ]
 
 
-def test_positive__parse_tags(positive__messages_for_parse_tags):
+@pytest.mark.parametrize('message,expected', testdata)
+def test_positive__parse_tags(message: str, expected: typing.List[str]):
     """
     Позитивный тест.
     Парсер тегов.
     """
-    for test_data in positive__messages_for_parse_tags:
-        assert parse_tags(test_data.input) == test_data.output
+    assert message_parsers.parse_tags(message) == expected
+
+
+testdata = \
+    [
+        (
+            'server #test', ['server', 'test']
+        ),
+        (
+            '#server#test', ['server', 'test']
+        ),
+        (
+            'server test', ['server', 'test']
+        )
+    ]
+
+
+@pytest.mark.parametrize('message,expected', testdata)
+def test_positive__search_by_tags(message: str, expected: typing.List[str]):
+    """
+    Позитивный тест
+    Парсер сообщения для поиска по тегам
+    :param positive_parse_message__search_by_tags:
+    :return:
+    """
+    message = Message(text=message, html_text=message)
+    assert message_parsers.parse_message__search_by_tags(message) == expected
+
+
+testdata = \
+    [
+        ('/add server', 'server'),
+        ('server', 'server'),
+        ('/add server $/asdf3$', 'server $/asdf3$')
+    ]
+
+
+@pytest.mark.parametrize('message,expected', testdata)
+def test_positive__cut_command(message: str, expected: str):
+    assert message_parsers.cut_command(message) == expected
