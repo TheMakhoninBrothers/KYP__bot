c1f6361ec5963d8a7c3a3c277c7343f35d9465c7
ofry <speitshot@gmail.com>
2022-01-05 18:16:49 +0300

    Добавлен новый запрос для поиска записей по тегам
    - удалены подзапросы

--- app/db/queries.py
+++ app/db/queries.py
@@ -1,4 +1,6 @@
-from . import Session, User, UserFromTelegram, Record
+from sqlalchemy import func
+
+from . import Session, User, UserFromTelegram, Record, Tag
 
 
 def find_user_by__chat_id(chat_id: int):
@@ -9,3 +11,13 @@ def find_user_by__chat_id(chat_id: int):
 def find_records_by__user(user: User):
     """Создание query Record по user_module (user_id)"""
     return Session().query(Record).filter(Record.user == user)
+
+
+def find_records__by_tags(user, *tags):
+    """Создание query Record по тегам"""
+    return Session(). \
+        query(Record, func.count(Record.id)). \
+        join(Tag, Record.tags). \
+        filter(Tag.name.in_(tags), Record.user == user). \
+        group_by(Record.id). \
+        having(func.count(Record.id) == len(tags))
